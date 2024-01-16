6230b594dce358f9d24c49cd03fa6c3208af0b26
James Stott <japstott@gmail.com>
2024-01-16 16:19:25 +0000

    create timer functionality when quiz starts

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -4,6 +4,11 @@ var endScreenSection = document.getElementById("end-screen");
 var feedbackSection = document.getElementById("feedback");
 
 var startButton = document.getElementById("start");
+var timerTextSection = document.getElementById("time");
+
+var timer = undefined;
+var timeLeft = 0;
+var penalty = false;
 
 startButton.addEventListener("click", function () {
     startGame();
@@ -12,6 +17,8 @@ startButton.addEventListener("click", function () {
 function startGame() {
     hideSection(startScreenSection);
     showSection(questionsSection);
+
+    startTimer();
 }
 
 function hideSection(section) {
@@ -20,4 +27,29 @@ function hideSection(section) {
 
 function showSection(section) {
     section.classList.remove("hide");
+}
+
+function startTimer() {
+    timeLeft = 10;
+
+    timerTextSection.textContent = timeLeft;
+
+    timer = setInterval(function () {
+        if (penalty) {
+            timeLeft = timeLeft - 10;
+            penalty = false;
+        }
+
+        timeLeft = Math.max(timeLeft - 1, 0);
+
+        if (timeLeft === 0) {
+            endGame();
+        }
+
+        timerTextSection.textContent = timeLeft;
+    }, 1000);
+}
+
+function endGame() {
+    clearInterval(timer);
 }
