1fa86b71ec7653081ac5234b03271e3ef31fea69
James Stott <japstott@gmail.com>
2024-01-16 15:59:26 +0000

    create variables to reference sections on home page

---
+++ assets/js/logic.js
@@ -0,0 +1,4 @@
+var startScreenSection = document.getElementById("start-screen");
+var questionsSection = document.getElementById("questions");
+var endScreenSection = document.getElementById("end-screen");
+var feedbackSection = document.getElementById("feedback");
