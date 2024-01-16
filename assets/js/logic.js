35e883872b0262a2767d21cb640005d3b50876b1
James Stott <japstott@gmail.com>
2024-01-16 16:09:50 +0000

    respond to start quiz button click

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -2,3 +2,22 @@ var startScreenSection = document.getElementById("start-screen");
 var questionsSection = document.getElementById("questions");
 var endScreenSection = document.getElementById("end-screen");
 var feedbackSection = document.getElementById("feedback");
+
+var startButton = document.getElementById("start");
+
+startButton.addEventListener("click", function () {
+    startGame();
+});
+
+function startGame() {
+    hideSection(startScreenSection);
+    showSection(questionsSection);
+}
+
+function hideSection(section) {
+    section.classList.add("hide");
+}
+
+function showSection(section) {
+    section.classList.remove("hide");
+}
