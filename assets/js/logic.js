1fbc731af6bc2e1b8b9dac7b90b79032e215e6c9
James Stott <japstott@gmail.com>
2024-01-16 16:38:44 +0000

    add questions for quiz

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -18,6 +18,8 @@ function startGame() {
     hideSection(startScreenSection);
     showSection(questionsSection);
 
+    shuffle(questions);
+
     startTimer();
 }
 
@@ -53,3 +55,21 @@ function startTimer() {
 function endGame() {
     clearInterval(timer);
 }
+
+function shuffle(array) {
+    let currentIndex = array.length,  randomIndex;
+  
+    // While there remain elements to shuffle.
+    while (currentIndex > 0) {
+  
+      // Pick a remaining element.
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex--;
+  
+      // And swap it with the current element.
+      [array[currentIndex], array[randomIndex]] = [
+        array[randomIndex], array[currentIndex]];
+    }
+  
+    return array;
+  }
